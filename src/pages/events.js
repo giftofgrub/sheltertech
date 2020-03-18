a55c13dd72fcdb8cd30c6c15b00491b017f15ddd
Gabriel <gab.atienza@gmail.com>
2020-03-18 16:11:06 -0700

    start Events page

---
+++ src/pages/events.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layouts'
+
+export default () => {
+   return(
+    <Layout>
+      <div>
+        <h1>Events</h1>
+      </div>
+    </Layout>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,8 @@ import React from 'react'
 import Home from '../components/Home'
 import Layout from '../components/layouts'
 
-
-
-export default ({ data }) => {
-  // Define the Blog Home & Blog Post content returned from Prismic
-  // const doc = data.prismic.allBlog_homes.edges.slice(0,1).pop();
-  // const posts = data.prismic.allPosts.edges;
-
-
-  return(
+export default () => {
+   return(
     <Layout>
       <Home />
     </Layout>
